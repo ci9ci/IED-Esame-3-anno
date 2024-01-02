8751faf2308bc85576a19d419b0100f7f4fe7ac0
ci9ci <ecossialice@gmail.com>
2024-01-02 18:01:28 +0100

    homeview allineato tutti gli elementi

--- app_flutter/lib/src/presentation/home/views/home_view.dart
+++ app_flutter/lib/src/presentation/home/views/home_view.dart
@@ -57,93 +57,130 @@ class _HomeConnectorState extends State<_HomeConnector> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  'Ciao ${user?.userMetadata?['user_name']},',
-                  style: GoogleFonts.figtree(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 22,
-                    color: const Color.fromRGBO(194, 232, 255, 1),
-                  ),
-                ),
-                Text(
-                  "Come posso aiutarti?",
-                  style: GoogleFonts.figtree(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 24,
-                    color: const Color.fromRGBO(194, 232, 255, 1),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                      top: 40.0,
-                      right: 8,
-                      left: 8), // Aggiungi spazio nella parte superiore
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8.0),
-                      border: Border.all(
-                        color: const Color.fromRGBO(194, 232, 255, 1),
-                        width: 2.0,
-                      ),
-                    ),
-                    height: 120.0,
-                    child: TextField(
-                      controller: controller,
-                      maxLines: null,
-                      style: const TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                        hintText:
-                            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
-                            'In tellus urna, pretium eget rutrum in, imperdiet ac lorem. Nunc eget luctus orci, quis imperdiet nulla.',
-                        hintStyle:
-                            TextStyle(color: Colors.white.withOpacity(0.5)),
-                        border: InputBorder.none,
-                        contentPadding: const EdgeInsets.symmetric(
-                            horizontal: 16.0, vertical: 16.0),
-                      ),
-                    ),
-                  ),
-                ),
-                BlocBuilder<HomeCubit, HomeState>(
-                  builder: (context, state) {
-                    if (state is HomeLoading) {
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-                    return Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
                       children: [
-                        IconButton(
-                          onPressed: () {
-                            context
-                                .read<HomeCubit>()
-                                .sendQuestion(controller.text);
-                          },
-                          icon: const Icon(Icons.send),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Ciao ${user?.userMetadata?['user_name']},',
+                                style: GoogleFonts.figtree(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 22,
+                                  color: const Color.fromRGBO(194, 232, 255, 1),
+                                ),
+                              ),
+                              Text(
+                                "Come posso aiutarti?",
+                                style: GoogleFonts.figtree(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 24,
+                                  color: const Color.fromRGBO(194, 232, 255, 1),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(width: 8.0),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromRGBO(194, 232, 255, 1),
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          padding: const EdgeInsets.only(
+                              right: 16, left: 16, top: 8, bottom: 8),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              const Icon(
+                                Icons.cloud_upload,
+                                size: 20.0,
+                              ),
+                              const SizedBox(width: 8.0),
+                              Text(
+                                'Upload',
+                                style: GoogleFonts.figtree(
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ],
-                    );
-                  },
+                    ),
+                  ],
                 ),
-                BlocBuilder<HomeCubit, HomeState>(
-                  builder: (context, state) {
-                    if (state is HomeError) {
-                      return Text('Error: ${state.error}');
-                    }
-                    if (state is HomeLoaded) {
-                      return Column(
-                        children: [
-                          Image.network(state.answer.imageURL),
-                          Text(state.answer.story),
-                        ],
-                      );
-                    }
-                    return const SizedBox();
-                  },
+                const SizedBox(
+                    height:
+                        16.0), // Aggiungi spazio tra il container e il TextField
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.0),
+                    border: Border.all(
+                      color: const Color.fromRGBO(194, 232, 255, 1),
+                      width: 2.0,
+                    ),
+                  ),
+                  height: 150.0,
+                  child: TextField(
+                    controller: controller,
+                    maxLines: null,
+                    style: const TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      hintText:
+                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+                          'In tellus urna, pretium eget rutrum in, imperdiet ac lorem. Nunc eget luctus orci, quis imperdiet nulla.',
+                      hintStyle:
+                          TextStyle(color: Colors.white.withOpacity(0.5)),
+                      border: InputBorder.none,
+                      contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 16.0, vertical: 16.0),
+                    ),
+                  ),
                 ),
               ],
             ),
+          ),
+          BlocBuilder<HomeCubit, HomeState>(
+            builder: (context, state) {
+              if (state is HomeLoading) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      context.read<HomeCubit>().sendQuestion(controller.text);
+                    },
+                    icon: const Icon(Icons.send),
+                  ),
+                ],
+              );
+            },
+          ),
+          BlocBuilder<HomeCubit, HomeState>(
+            builder: (context, state) {
+              if (state is HomeError) {
+                return Text('Error: ${state.error}');
+              }
+              if (state is HomeLoaded) {
+                return Column(
+                  children: [
+                    Image.network(state.answer.imageURL),
+                    Text(state.answer.story),
+                  ],
+                );
+              }
+              return const SizedBox();
+            },
           ),
         ],
       ),
